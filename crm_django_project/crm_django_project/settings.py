d06a503d02a831b55ceb4044aa986618894c9883
1Pvvv <potap.iv@pm.me>
2024-09-05 23:02:06 +0300

    change default DB to postgreSQL

--- crm_django_project/crm_django_project/settings.py
+++ crm_django_project/crm_django_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "crm_django_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crm_db",
+        "USER": "crm_user",
+        "PASSWORD": "password",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
@@ -122,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
